12de68d43a561c4a50973e4c5fbfc4a4ea1efe08
Asmaa Eliwa <asmaagamalelsayed96@gmail.com>
2024-03-17 15:04:20 +0200

    setting up app

---
+++ MyMapApp/MyMapAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyMapAppApp.swift
+//  MyMapApp
+//
+//  Created by asmaa gamal  on 17/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct MyMapAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
